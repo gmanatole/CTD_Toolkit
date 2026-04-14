01943480f71e37f0f4781a720320ab21aa6b0dbb
GROS MARTIAL Anatole <m1_gui01@cebc.cnrs.local>
2026-04-14 18:09:15 +0200

    FCPA code - first working version

--- src/ctd_toolkit/functional_principal_component.py
+++ src/ctd_toolkit/functional_principal_component.py
@@ -8,13 +8,13 @@ from skfda.representation.basis import BSplineBasis
 from skfda.preprocessing.smoothing import BasisSmoother
 from skfda.misc.regularization import L2Regularization
 from skfda.misc.operators import LinearDifferentialOperator
+from scipy.linalg import cholesky, solve_triangular
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 
 class FunctionalPrincipalComponent:
 
-    K = 100
+    K = 50
     lam = 0.05
 
     def __init__(self,
@@ -44,26 +44,32 @@ class FunctionalPrincipalComponent:
 
     @property
     def depth_min(self):
-        return self.__depth_max
+        return self.__depth_min
 
     @depth_min.setter
     def depth_min(self, value):
         self.__depth_min = value
 
-    def corresponding_files(self):
-
-        bounds = self._extract_grid_bounds()
-        sql = self._file_query(bounds=bounds)
-        df = self.source.query(sql)
-        return df
-
     def filter_depth(self) :
-        pass
+        depth, data = [], []
+        for _depth, _data in zip(self.depth, self.data):
+            _depth = _depth.flatten()
+            _data = _data.flatten()
+            if (np.nanmax(_depth) > self.depth_max) & (np.nanmin(_depth) < self.depth_min):
+                depth_range = np.arange(int(np.floor(np.nanmin(_depth))), np.nanmax(_depth) + 1)
+                _data_interp = np.interp(depth_range, _depth, _data)
+                _depth_interp = np.arange(self.depth_min, self.depth_max + 1, 1).astype(int)
+                _data_interp = _data_interp[np.isin(depth_range, _depth_interp)]
+                if np.isnan(_data_interp).sum() > 0:
+                    continue
+                depth.append(_depth_interp)
+                data.append(_data_interp)
+        self.data = np.array(data)
+        self.depth = np.array(depth)
 
     def base_projection(self) :
         # Create bspline base
         self.basis = BSplineBasis(domain_range=(self.depth_min, self.depth_max), n_basis=self.K, order=4)
-        self.data = self.data[self.depth_min:self.depth_max + 1, :].T
         # Project data to base, shape should be (Nobs x Npoints)
         fd = FDataGrid(data_matrix=self.data, grid_points=np.arange(self.depth_min, self.depth_max + 1))
         # Lambda penalty on second derivative
@@ -72,22 +78,76 @@ class FunctionalPrincipalComponent:
             regularization_parameter=self.lam)
         smoother = BasisSmoother(
             basis=self.basis,
-            regularization=regularization)
+            regularization=regularization,
+            return_basis=True)
         self.ydf = smoother.fit_transform(fd)
 
+    def fpca_hilbert(self):
+        self.alpha = self.ydf.coefficients
+        alpha_mean = self.alpha.mean(axis=0)
+        alpha_std = self.alpha.std(axis=0)
+        n = self.alpha.shape[0]
+        C = self.alpha - alpha_mean
+        C_norm = (self.alpha - alpha_mean) / alpha_std
+        mat_cov = (C.T @ C) / n
+        mat_cor = (C_norm.T @ C_norm) / n
+        W = self.basis.gram_matrix()
+        self.W = (W + W.T) / 2
+        Wdem = cholesky(W)
+        Wdeminv = solve_triangular(Wdem, np.eye(Wdem.shape[0]))
+        eigenvalues, eigenvectors = np.linalg.eig(Wdem @ mat_cov @ Wdem.T)
+
+        # Handle complex numbers if needed
+        eigenvalues = np.real(eigenvalues)
+        eigenvectors = np.real(eigenvectors)
+        # Handle negative eigenvalues
+        eigenvalues = np.abs(eigenvalues)
+
+        order = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[order]
+        eigenvectors = eigenvectors[:, order]
+        vectnotWM = eigenvectors.copy()  # b_k (not W-normalized)
+        vectors = Wdeminv @ vectnotWM
+
+        axe = vectors * np.sqrt(eigenvalues)  # deformation axes
+        pc = C @ self.W @ vectors  # principal components, shape: (Nobs, K)
+        pval = np.round(eigenvalues / eigenvalues.sum() * 100, 3)
+
+        self.mfpca = {
+            'values': eigenvalues,
+            'vectors': vectors,
+            'vectnotWM': vectnotWM,
+            'axe': axe,
+            'pc': pc,
+            'pval': pval}
+
     def visualize_projection(self) :
-        idx = random.sample(range(0, self.data.shape[1] - 1), 4)
-        y = list(range(0, self.data.shape[0] - 1))
-        x = self.basis(y)
-        fig, ax = plt.subplots(2,2, sharex = True, sharey = True)
-        for i, _idx in enumerate(idx) :
-            ax[i].scatter(x @ self.yfd.coefficients[_idx, :], x)
-            ax[i].scatter(self.data[:, _idx], x)
-            ax[i].invert_yaxis()
-        fig.show()
+        idx = np.random.choice(self.data.shape[0], 10, replace=False)
+        xgrid = np.arange(self.depth_min, self.depth_max + 1)
+        # Evaluate basis on grid - equivalent to eval.basis()
+        ygrid = self.basis.to_basis()(xgrid)
+        # shape: (n_basis, n_points)
+        coefficients = self.ydf.coefficients  # shape: (Nobs, K)
+        fig, axes = plt.subplots(2, 5, figsize=(15, 8))
+        axes = axes.flatten()
+        for i, k in enumerate(idx):
+            ylabel = self.data[k, :]
+            ypred = ygrid.T @ coefficients[k]  # ygrid %*% yfd$coefs[, k]
+            axes[i].plot(ylabel, -xgrid, color='maroon', label='observed')
+            axes[i].plot(ypred.flatten(), -xgrid, color='navy', label='fitted')
+            axes[i].set_title(f'Obs {k}')
+            axes[i].legend()
+            plt.tight_layout()
+            plt.show()
+
+    def corresponding_files(self):
+
+        bounds = self._extract_grid_bounds()
+        sql = self._file_query(bounds=bounds)
+        df = self.source.query(sql)
+        return df
 
     def get_profiles(self, df, workers = 8) :
-
         # group by file
         file_groups = list(df.groupby("fn"))
         # prepare tasks for workers
@@ -95,12 +155,14 @@ class FunctionalPrincipalComponent:
             (fn, group.reset_index(drop=True), self.var)
             for fn, group in file_groups
         ]
-        data = []
+        depth, data = [], []
         with ProcessPoolExecutor(max_workers=workers) as executor:
             futures = executor.map(_process_file_reader, tasks)
             for result in tqdm(futures, total=len(tasks), desc="Processing files"):
-                data.append(result)
+                data.extend(result[1])
+                depth.extend(result[0])
         self.data = data
+        self.depth = depth
 
     def _extract_grid_bounds(self):
 
